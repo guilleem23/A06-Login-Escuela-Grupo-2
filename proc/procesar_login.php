18e0461edc6cd582ec439edab003bc0b04c3b7e9
junhao2005 <145134984+junhao2005@users.noreply.github.com>
2025-10-30 18:26:18 +0100

    s0

--- proc/procesar_login.php
+++ proc/procesar_login.php
@@ -45,7 +45,7 @@ if ($ok) {
     header('Location: ../index.php');
     exit;
 } else {
-    // contraseña incorrectaaaaaaaa
+    // contraseña incorrea
     header('Location: ../login.php?error=credenciales_invalidas');
     exit;
 }
